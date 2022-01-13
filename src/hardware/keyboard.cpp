6b2c0280e8e9a499d208709efd051b7b4f454cb4
Akamaki <97360908+akmed772@users.noreply.github.com>
2022-01-13 22:45:10 +0900

    Add keyboard support for scancode set 81h

--- src/hardware/keyboard.cpp
+++ src/hardware/keyboard.cpp
@@ -116,7 +116,7 @@ static Bitu read_p60(Bitu /*port*/,Bitu /*iolen*/) {
 }
 
 static void write_p60(Bitu /*port*/,Bitu val,Bitu /*iolen*/) {
-	//LOG(LOG_KEYBOARD, LOG_NORMAL)("Port 60 write with val %" sBitfs(X), val);
+	LOG(LOG_KEYBOARD, LOG_NORMAL)("Port 60 write with val %" sBitfs(X), val);
 	switch (keyb.command) {
 	case CMD_NONE:	/* None */
 		/* No active command this would normally get sent to the keyboard then */
@@ -137,8 +137,8 @@ static void write_p60(Bitu /*port*/,Bitu val,Bitu /*iolen*/) {
 			/* AT's just send acknowledge */
 			if (IS_PS55_ARCH) {
 				//for PS/55
-				KEYBOARD_AddBuffer(0x90);//90 AB = 5576-002
 				KEYBOARD_AddBuffer(0xAB);
+				KEYBOARD_AddBuffer(0x90);//90 AB = 5576-002
 			}
 			KEYBOARD_AddBuffer(0xfa);	/* Acknowledge */
 			break;
@@ -204,16 +204,20 @@ static void write_p60(Bitu /*port*/,Bitu val,Bitu /*iolen*/) {
 			KEYBOARD_AddBuffer(0xfa);	/* Acknowledge */
 			LOG(LOG_KEYBOARD, LOG_NORMAL)("Switched to scancode set 8A");
 		}
-		//else if (val <= 2) {
-		//	keyb.scancodeset = val;
-		//	keyb.repeat.rate = 33;
-		//	KEYBOARD_AddBuffer(0xfa);	/* Acknowledge */
-		//	LOG(LOG_KEYBOARD, LOG_NORMAL)("Switched to default scancode set");
-		//}
-		else KEYBOARD_AddBuffer(0xfe); /* Resend */
+		else if (val == 0x81) {
+			keyb.scancodeset = val;
+			KEYBOARD_AddBuffer(0xfa);	/* Acknowledge */
+			LOG(LOG_KEYBOARD, LOG_NORMAL)("Switched to scancode set 81");
+		}
+		else
+		{
+			KEYBOARD_AddBuffer(0xfe); /* Resend */
+			return;
+		}
 		keyb.repeat.key = KBD_NONE;
 		keyb.repeat.pause = 500;
 		keyb.repeat.wait = 0;
+		keyb.command = CMD_NONE;
 		break;
 	}
 }
@@ -404,22 +408,12 @@ void KEYBOARD_AddKey(KBD_KEYS keytype, bool pressed) {
 		case KBD_insert:ret = 0x4f; break;
 		case KBD_delete:ret = 0x50; break;
 		case KBD_pause:ret = 0x47; break;//126
-/*			KEYBOARD_AddBuffer(0xe1);
-			KEYBOARD_AddBuffer(29 | (pressed ? 0 : 0x80));
-			KEYBOARD_AddBuffer(69 | (pressed ? 0 : 0x80));
-			return;*/
 		case KBD_printscreen: ret = 0x74; break;//124
-/*			KEYBOARD_AddBuffer(0xe0);
-			KEYBOARD_AddBuffer(42 | (pressed ? 0 : 0x80));
-			KEYBOARD_AddBuffer(0xe0);
-			KEYBOARD_AddBuffer(55 | (pressed ? 0 : 0x80));
-			return;*/
-			//case KBD_underscore:ret = 0x73; break;//for JP layout
-			//case KBD_yen:ret = 0x7d; break;//for JP layout
-		case KBD_backslash:ret = 0x0b; break;//for AX layout
-		case KBD_yen:ret = 0x30; break;//for AX layout
-		case KBD_conv:ret = 0x35; break;//for AX layout
-		case KBD_nconv:ret = 0x33; break;//for AX layout
+		case KBD_backslash:ret = 0x0b; break;//for AX/JP layout
+		case KBD_yen:ret = 0x30; break;//for AX/JP layout
+		case KBD_conv:ret = 0x35; break;//for AX/JP layout
+		case KBD_nconv:ret = 0x33; break;//for AX/JP layout
+		case KBD_ax:ret = 0; break;//AX
 		case KBD_hankaku:ret = 0x45; break;//for PS/55
 		case KBD_kana:relbreak = true; ret = 0x36; break;//for PS/55
 
@@ -493,6 +487,187 @@ void KEYBOARD_AddKey(KBD_KEYS keytype, bool pressed) {
 			else
 				return;//most keys send nothing when the key is released
 		}
+	}
+	else if (IS_PS55_ARCH)
+	{
+		bool extend = false;
+		switch (keytype) {
+		case KBD_esc:ret = 1; break;
+		case KBD_1:ret = 2; break;
+		case KBD_2:ret = 3; break;
+		case KBD_3:ret = 4; break;
+		case KBD_4:ret = 5; break;
+		case KBD_5:ret = 6; break;
+		case KBD_6:ret = 7; break;
+		case KBD_7:ret = 8; break;
+		case KBD_8:ret = 9; break;
+		case KBD_9:ret = 10; break;
+		case KBD_0:ret = 11; break;
+
+		case KBD_minus:ret = 12; break;
+		case KBD_equals:ret = 13; break;
+		case KBD_backspace:ret = 14; break;
+		case KBD_tab:ret = 15; break;
+
+		case KBD_q:ret = 16; break;
+		case KBD_w:ret = 17; break;
+		case KBD_e:ret = 18; break;
+		case KBD_r:ret = 19; break;
+		case KBD_t:ret = 20; break;
+		case KBD_y:ret = 21; break;
+		case KBD_u:ret = 22; break;
+		case KBD_i:ret = 23; break;
+		case KBD_o:ret = 24; break;
+		case KBD_p:ret = 25; break;
+
+		case KBD_leftbracket:ret = 26; break;
+		case KBD_rightbracket:ret = 27; break;
+		case KBD_enter:ret = 28; break;
+		case KBD_leftctrl:ret = 29; break;
+
+		case KBD_a:ret = 30; break;
+		case KBD_s:ret = 31; break;
+		case KBD_d:ret = 32; break;
+		case KBD_f:ret = 33; break;
+		case KBD_g:ret = 34; break;
+		case KBD_h:ret = 35; break;
+		case KBD_j:ret = 36; break;
+		case KBD_k:ret = 37; break;
+		case KBD_l:ret = 38; break;
+
+		case KBD_semicolon:ret = 39; break;
+		case KBD_quote:ret = 40; break;
+		case KBD_grave:ret = 41; break;//29h `~Ñ£
+		case KBD_leftshift:ret = 42; break;
+			//case KBD_backslash:ret = 43; break;//2bh \|°
+		case KBD_z:ret = 44; break;
+		case KBD_x:ret = 45; break;
+		case KBD_c:ret = 46; break;
+		case KBD_v:ret = 47; break;
+		case KBD_b:ret = 48; break;
+		case KBD_n:ret = 49; break;
+		case KBD_m:ret = 50; break;
+
+		case KBD_comma:ret = 51; break;
+		case KBD_period:ret = 52; break;
+		case KBD_slash:ret = 53; break;
+		case KBD_rightshift:ret = 54; break;
+		case KBD_kpmultiply:ret = 55; break;
+		case KBD_leftalt:ret = (keyb.scancodeset == 0x81) ? 0x70 : 56; break;//for PS/55
+		case KBD_space:ret = 57; break;
+		case KBD_capslock:ret = 58; break;
+
+		case KBD_f1:ret = 59; break;
+		case KBD_f2:ret = 60; break;
+		case KBD_f3:ret = 61; break;
+		case KBD_f4:ret = 62; break;
+		case KBD_f5:ret = 63; break;
+		case KBD_f6:ret = 64; break;
+		case KBD_f7:ret = 65; break;
+		case KBD_f8:ret = 66; break;
+		case KBD_f9:ret = 67; break;
+		case KBD_f10:ret = 68; break;
+
+		case KBD_numlock:ret = 69; break;
+		case KBD_scrolllock:ret = 70; break;
+
+		case KBD_kp7:ret = 71; break;
+		case KBD_kp8:ret = 72; break;
+		case KBD_kp9:ret = 73; break;
+		case KBD_kpminus:ret = 74; break;
+		case KBD_kp4:ret = 75; break;
+		case KBD_kp5:ret = 76; break;
+		case KBD_kp6:ret = 77; break;
+		case KBD_kpplus:ret = 78; break;
+		case KBD_kp1:ret = 79; break;
+		case KBD_kp2:ret = 80; break;
+		case KBD_kp3:ret = 81; break;
+		case KBD_kp0:ret = 82; break;
+		case KBD_kpperiod:ret = 83; break;
+
+		case KBD_extra_lt_gt:ret = 86; break;//56h \|Û underscore
+		case KBD_f11:ret = 87; break;
+		case KBD_f12:ret = 88; break;
+
+			//The Extended keys
+
+		case KBD_kpenter:extend = true; ret = 28; break;
+		case KBD_rightctrl:
+		{
+			extend = true;
+			ret = 29;//0x1d
+			break;
+		}
+		case KBD_kpdivide:extend = true; ret = 53; break;
+		case KBD_rightalt:
+		{
+			if (keyb.scancodeset != 0x81) extend = true;
+			ret = 56;//0x38
+			break;
+		}
+		//case KBD_rightalt: ret = 0xA7; break; ax
+		case KBD_home:extend = true; ret = 71; break;
+		case KBD_up:extend = true; ret = 72; break;
+		case KBD_pageup:extend = true; ret = 73; break;
+		case KBD_left:extend = true; ret = 75; break;
+		case KBD_right:extend = true; ret = 77; break;
+		case KBD_end:extend = true; ret = 79; break;
+		case KBD_down:extend = true; ret = 80; break;
+		case KBD_pagedown:extend = true; ret = 81; break;
+		case KBD_insert:extend = true; ret = 82; break;
+		case KBD_delete:extend = true; ret = 83; break;
+		case KBD_pause:
+			KEYBOARD_AddBuffer(0xe1);
+			KEYBOARD_AddBuffer(29 | (pressed ? 0 : 0x80));
+			KEYBOARD_AddBuffer(69 | (pressed ? 0 : 0x80));
+			return;
+		case KBD_printscreen:
+			KEYBOARD_AddBuffer(0xe0);
+			KEYBOARD_AddBuffer(42 | (pressed ? 0 : 0x80));
+			KEYBOARD_AddBuffer(0xe0);
+			KEYBOARD_AddBuffer(55 | (pressed ? 0 : 0x80));
+			return;
+			//case KBD_underscore:ret = 0x73; break;//for JP layout
+			//case KBD_yen:ret = 0x7d; break;//for JP layout
+		case KBD_backslash:ret = 86; break;//for AX/JP layout
+		case KBD_yen:ret = 43; break;//for AX/JP layout
+		case KBD_conv:ret = (keyb.scancodeset == 0x81) ? 0x79 : 57; break;
+		case KBD_nconv:ret = (keyb.scancodeset == 0x81) ? 0x7b : 57; break;
+		case KBD_ax:ret = 0; break;//AX
+		case KBD_hankaku:ret = (keyb.scancodeset == 0x81) ? 0x77 : 127; break;//for PS/55
+		case KBD_kana:
+			if (keyb.scancodeset == 0x81) {
+				extend = true; ret = 0x38;
+			}
+			else
+				ret = 57;
+			break;
+		case KBD_kpcomma:
+			if (keyb.scancodeset == 0x81) {
+				extend = true; ret = 51;//scancode set 81 for PS/55
+			}
+			else
+				ret = 0;//scancode set 01 for PS/55
+			break;
+		default:
+			E_Exit("Unsupported key press");
+			break;
+		}
+		/* Add the actual key in the keyboard queue */
+		if (pressed) {
+			if (keyb.repeat.key == keytype) keyb.repeat.wait = keyb.repeat.rate;
+			else keyb.repeat.wait = keyb.repeat.pause;
+			keyb.repeat.key = keytype;
+		}
+		else {
+			if (keyb.repeat.key == keytype) {
+				/* repeated key being released */
+				keyb.repeat.key = KBD_NONE;
+				keyb.repeat.wait = 0;
+			}
+			ret += 128;
+		}
+		if (extend) KEYBOARD_AddBuffer(0xe0);
 	}
 	else
 	{
@@ -635,10 +810,10 @@ void KEYBOARD_AddKey(KBD_KEYS keytype, bool pressed) {
 			return;
 			//case KBD_underscore:ret = 0x73; break;//for JP layout
 			//case KBD_yen:ret = 0x7d; break;//for JP layout
-		case KBD_backslash:ret = 86; break;//for AX layout
-		case KBD_yen:ret = 43; break;//for AX layout
-		case KBD_conv:ret = (INT16_AX_GetKBDBIOSMode() == 0x51) ? 0x5b : 57; break;//for AX layout
-		case KBD_nconv:ret = (INT16_AX_GetKBDBIOSMode() == 0x51) ? 0x5a : 57; break;//for AX layout
+		case KBD_backslash:ret = 86; break;//for AX/JP layout
+		case KBD_yen:ret = 43; break;//for AX/JP layout
+		case KBD_conv:ret = (INT16_AX_GetKBDBIOSMode() == 0x51) ? 0x5b : 57; break;//for AX/JP layout
+		case KBD_nconv:ret = (INT16_AX_GetKBDBIOSMode() == 0x51) ? 0x5a : 57; break;//for AX/JP layout
 		case KBD_ax:ret = (INT16_AX_GetKBDBIOSMode() == 0x51) ? 0x5c : 0; break;//for AX layout
 			/* System Scan Code for AX keys
 			JP mode  Make    Break    US mode  Make    Break
@@ -656,7 +831,7 @@ void KEYBOARD_AddKey(KBD_KEYS keytype, bool pressed) {
 			*/
 		case KBD_hankaku:ret = 127; break;//scancode set 01 for PS/55
 		case KBD_kana: ret = 57; break;//scancode set 01 for PS/55
-		case KBD_kpcomma:extend = true; ret = 51; break;//scancode set 01 for PS/55
+		case KBD_kpcomma: ret = 0; break;//scancode set 01 for PS/55
 		default:
 			E_Exit("Unsupported key press");
 			break;
